82279547301ba2331617559789b334e7e4a61b8f
NilKad <kadulin@gmail.com>
2024-05-03 14:42:01 +0200

    fix

--- query_9.sql
+++ query_9.sql
@@ -1,4 +1,4 @@
-select s.id as subkject_id, s."name" as subject_name 
+select s.id as subject_id, s."name" as subject_name 
 from grades g 
 join subjects s on s.id = g.subject_id 
 where g.student_id  = 32
